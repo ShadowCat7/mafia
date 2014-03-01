03dd0b5895ef2ed8c4c6b1e7cae534d149af1a2f
shadowcat7 <lukasattridge@gmail.com>
2014-03-01 14:22:45 -0700

    temp view engine update

--- src/viewengine.js
+++ src/viewengine.js
@@ -3,11 +3,11 @@ var fileSystem = require('fs');
 var anyPattern = /\@(?!!)\(.*?\)/;
 var layoutPattern = /^layout\=/i;
 var dataPattern = /^data\./i;
+var partialViewPattern = /^partialFor\./i;
 
 function getView(viewName, viewData, callback) {
 	fileSystem.readFile('views/' + viewName, 'utf-8', function (fileError, fileData) {
-		if (fileError)
-			throw fileError;
+		if (fileError) throw fileError;
 
 		fillView(fileData, viewData, callback);
 	});
@@ -22,24 +22,42 @@ function fillView(view, viewData, callback) {
 	else {
 		match = match[0].slice(2, match[0].length - 1);
 
-		if (layoutPattern.test(match))
+		if (layoutPattern.test(match)) {
 			getLayout(match.slice(7, match.length), view, viewData, callback);
-		else if (dataPattern.test(match))
+		}
+		else if (dataPattern.test(match)) {
 			fillData(match.slice(5, match.length), view, viewData, callback);
+		}
+		else if (partialViewPattern.test(match)) {
+			match.slice(
+		}
 	}
 }
 
 function getLayout(layoutName, view, viewData, callback) {
 	fileSystem.readFile('views/' + layoutName, 'utf-8', function (fileError, fileData) {
-		if (fileError)
-			throw fileError;
+		if (fileError) throw fileError;
 
 		fillView(fileData.replace(anyPattern, view.replace(anyPattern, '')), viewData, callback);
 	}); 
 }
 
 function fillData(fieldName, view, viewData, callback) {
-	fillView(view.replace(anyPattern, viewData[fieldName]), viewData, callback);
+	var model = viewData[fieldName];
+
+	if (model instanceof Array) {
+		for (var i in model) {
+			fillView(view.replace(anyPattern, viewData[fieldName]), viewData, callback);
+		}
+	}
+	else {
+		fillView(view.replace(anyPattern, viewData[fieldName]), viewData, callback);
+	}
+
+}
+
+function fillPartialView(partialName, partialViewData, callback) {
+
 }
 
 module.exports = {
