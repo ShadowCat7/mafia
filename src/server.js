79633c0402333ae0194b4fe654e39d8ccb804cdf
Josh Wolfe <thejoshwolfe@gmail.com>
2014-03-01 18:58:52 -0700

    fixing hello world http server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 var http = require('http');
+var ws = require('ws');
 
 var port = 8080;
+
+var httpServer = http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('let\'s play some mafia!');
+});
+httpServer.listen(port);
 console.log('listening at http://127.0.0.1:' + port);
 
-http.createServer(function (request, response) {
-	request.on('end', function () {
-		response.end('let\'s play some mafia!');
-	});
-
-}).listen(port);
-
+var webSocketServer = new ws.Server({server: httpServer});
+webSocketServer.on('connection', function (socket) {
+  socket.on('message', function (data, flags) {
+    console.log('received data:', JSON.stringify(data));
+    socket.send('asdf');
+  });
+});
