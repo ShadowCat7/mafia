4924855351de314a2fc49f45228f627e23ca7241
Attridge <shado2@ubuntu.ubuntu-domain>
2014-03-01 19:52:52 -0700

    Change styling, add views

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
-var http = require('http');
-var ws = require('ws');
+var http = require("http");
+var ws = require("ws");
+var viewEngine = require("./viewengine");
 
 var port = 8080;
 
-var httpServer = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('let\'s play some mafia!');
+var httpServer = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+
+  viewEngine.getView("index.html", null, function(view) {
+    response.end(view);
+  });
 });
 httpServer.listen(port);
-console.log('listening at http://127.0.0.1:' + port);
+console.log("listening at http://127.0.0.1:" + port);
 
 var webSocketServer = new ws.Server({server: httpServer});
-webSocketServer.on('connection', function (socket) {
-  socket.on('message', function (data, flags) {
-    console.log('received data:', JSON.stringify(data));
-    socket.send('asdf');
+webSocketServer.on("connection", function(socket) {
+  socket.on("message", function(data, flags) {
+    console.log("received data:", JSON.stringify(data));
+    socket.send("asdf");
   });
 });
