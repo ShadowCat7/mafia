1d1474468ea84a84e12108a1b7cb3253ca64d0cd
Josh Wolfe <thejoshwolfe@gmail.com>
2014-03-02 22:32:28 -0700

    command/response architecture with login command

--- public/mafia.js
+++ public/mafia.js
@@ -1,4 +1,29 @@
 
+var loginDiv = document.getElementById("loginDiv");
+var loginNameTextbox = document.getElementById("loginNameTextbox");
+var loginButton = document.getElementById("loginButton");
+
+loginNameTextbox.addEventListener("keydown", function(event) {
+  if (event.keyCode === 13) {
+    // Enter
+    login();
+    event.preventDefault();
+  }
+});
+loginButton.addEventListener("click", login);
+function login() {
+  var name = loginNameTextbox.value;
+  name = name.trim();
+  if (name === "") return;
+
+  loginNameTextbox.setAttribute("disabled", "true");
+  loginButton.setAttribute("disabled", "true");
+  sendCommand("login", {name: name}, function(response) {
+    playerList.innerHTML += '<li>login result: ' + sanitizeHtml(JSON.stringify(response)) + '</li>';
+    loginDiv.style.display = "none";
+  });
+}
+
 var playerList = document.getElementById("playerList");
 
 var webSocket = null;
@@ -12,9 +37,32 @@ function connectWebSocket() {
   });
   webSocket.addEventListener("open", function() {
     playerList.innerHTML += '<li>websocket open</li>';
-    webSocket.send(JSON.stringify(["hello", "world"]));
   });
   webSocket.addEventListener("message", function(event) {
-    playerList.innerHTML += '<li>message received: ' + event.data + '</li>';
+    playerList.innerHTML += '<li>message received: ' + sanitizeHtml(event.data) + '</li>';
+    var message = JSON.parse(event.data);
+    receiveMessage(message);
   });
+}
+
+var nextMessageId = 0;
+var messageCallbacks = {};
+function sendCommand(commandName, args, callback) {
+  var cookie = nextMessageId++;
+  messageCallbacks[cookie] = callback;
+  var commandObject = {
+    command: commandName,
+    args: args,
+    cookie: cookie,
+  };
+  webSocket.send(JSON.stringify(commandObject));
+}
+function receiveMessage(message) {
+  var cookie = message.cookie;
+  var callback = messageCallbacks[cookie];
+  callback(message.response);
+}
+
+function sanitizeHtml(text) {
+  return text.replace(/&/g, "&amp;").replace(/</g, "&lt;");
 }
