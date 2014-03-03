ceb52e2fa8472533857153c75e7ff1447b740d45
Josh Wolfe <thejoshwolfe@gmail.com>
2014-03-02 18:06:36 -0700

    websocket stuff works

--- public/mafia.js
+++ public/mafia.js
@@ -1,3 +1,20 @@
-(function() {
 
-})();
+var playerList = document.getElementById("playerList");
+
+var webSocket = null;
+connectWebSocket();
+function connectWebSocket() {
+  var url = "ws://" + location.hostname + ":" + location.port + location.pathname;
+  webSocket = new WebSocket(url);
+playerList.innerHTML += '<li>websocket connecting...</li>';
+  webSocket.addEventListener("error", function() {
+    playerList.innerHTML += '<li>websocket error</li>';
+  });
+  webSocket.addEventListener("open", function() {
+    playerList.innerHTML += '<li>websocket open</li>';
+    webSocket.send("fdsa");
+  });
+  webSocket.addEventListener("message", function(event) {
+    playerList.innerHTML += '<li>message received: ' + event.data + '</li>';
+  });
+}
